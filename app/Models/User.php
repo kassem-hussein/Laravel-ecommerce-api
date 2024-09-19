936d9471621578100aa66f590c1d0052f01ddbf6
KASSEM HUSSEIN <kassem.learn@gmail.com>
2024-09-19 06:36:35 +0300

    add HasAPiToken trait to USER model

--- app/Models/User.php
+++ app/Models/User.php
@@ -7,10 +7,11 @@ use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Relations\HasMany;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
+use Laravel\sanctum\HasApiToken;
 
 class User extends Authenticatable
 {
-    use HasFactory, Notifiable;
+    use HasFactory, Notifiable,HasApiToken;
 
     /**
      * The attributes that are mass assignable.
