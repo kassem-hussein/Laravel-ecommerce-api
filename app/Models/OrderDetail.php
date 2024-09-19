40df6ac0d0c33a7138410f41eabcfa060dd63416
KASSEM HUSSEIN <kassem.learn@gmail.com>
2024-09-19 06:20:33 +0300

    Define Relationships

--- app/Models/OrderDetail.php
+++ app/Models/OrderDetail.php
@@ -4,10 +4,24 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
 
 class OrderDetail extends Model
 {
     use HasFactory;
     protected $table ="orders_details";
     protected $guarded = [];
+
+
+    public function product():BelongsTo{
+        return $this->belongsTo(Product::class,"product","id");
+    }
+    public function size():BelongsTo{
+        return $this->belongsTo(Size::class,"size","id");
+    }
+    public function color():BelongsTo{
+        return $this->belongsTo(Color::class,"color","id");
+    }
+
+
 }
--- app/Models/Product.php
+++ app/Models/Product.php
@@ -4,10 +4,28 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\HasMany;
+use Illuminate\Database\Eloquent\Relations\HasOne;
 
 class Product extends Model
 {
     use HasFactory;
     protected $table ="products";
     protected $guarded = [];
+
+    public function category():HasOne{
+        return $this->hasOne(Category::class,"product","id");
+    }
+    public function brand():HasOne{
+        return $this->hasOne(Brand::class,"product","id");
+    }
+    public function images():HasMany{
+        return $this->hasMany(ProductImage::class,"product","id");
+    }
+    public function colors():HasMany{
+        return $this->hasMany(Color::class,"product","id");
+    }
+    public function sizes():HasMany{
+        return $this->hasMany(Size::class,"product","id");
+    }
 }
