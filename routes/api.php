a0f463450f65ba9313898ff242b8c80c1c1dc12b
KASSEM HUSSEIN <kassem.learn@gmail.com>
2024-09-21 13:27:45 +0300

    Customize Api Route

--- routes/api.php
+++ routes/api.php
@@ -8,7 +8,6 @@ use App\Http\Controllers\ColorsController;
 use App\Http\Controllers\OrdersController;
 use App\Http\Controllers\ProductsController;
 use App\Http\Controllers\SizesController;
-use App\Http\Requests\BrandRequest;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -16,77 +15,76 @@ Route::get('/user', function (Request $request) {
     return $request->user();
 })->middleware('auth:sanctum');
 
+Route::middleware("auth:sanctum")->group(function(){
+    Route::controller(ColorsController::class)->prefix("/colors")->group(function(){
+        Route::get("/","index");
+        Route::post("/","store");
+        Route::get("/{id}","show");
+        Route::put("/{id}","update");
+        Route::delete("/{id}","destroy");
+    });
 
-Route::controller(ColorsController::class)->prefix("/colors")->group(function(){
-    Route::get("/","index");
-    Route::post("/","store");
-    Route::get("/{id}","show");
-    Route::put("/{id}","update");
-    Route::delete("/{id}","destroy");
+    Route::controller(CategoriesController::class)->prefix("/categories")->group(function(){
+        Route::get("/","index");
+        Route::post("/","store");
+        Route::get("/{id}","show");
+        Route::put("/{id}","update");
+        Route::delete("/{id}","destroy");
+    });
+
+    Route::controller(BrandController::class)->prefix("/brands")->group(function(){
+        Route::get("/","index");
+        Route::post("/","store");
+        Route::get("/{id}","show");
+        Route::put("/{id}","update");
+        Route::delete("/{id}","destroy");
+    });
+    Route::controller(SizesController::class)->prefix("/sizes")->group(function(){
+        Route::get("/","index");
+        Route::post("/","store");
+        Route::get("/{id}","show");
+        Route::put("/{id}","update");
+        Route::delete("/{id}","destroy");
+    });
+
+    Route::controller(OrdersController::class)->prefix("/orders")->group(function(){
+        Route::get("/","index");
+        Route::post("/","store");
+        Route::get("/{id}","show");
+        Route::get("/{id}/checkout","checkout");
+        Route::put("/{id}","updateStatus");
+        Route::delete("/{id}","destroy");
+    });
+
+
+    Route::controller(ProductsController::class)->prefix("/products")->group(function(){
+        Route::get("/","index");
+        Route::post("/","store");
+        Route::delete("/stocks/{id}","removeProductStock");
+        Route::get("/{id}","show");
+        Route::get("/{id}/images","productImages");
+        Route::post("/{id}/images","addProductImage");
+        Route::get("/{id}/stocks","productStocks");
+        Route::post("/{id}/stocks","addProductStock");
+        Route::put("/{id}","update");
+        Route::delete("/{id}","destroy");
+    });
+
+
+    Route::controller(AddressesController::class)->prefix("/addresses")->group(function(){
+        Route::get("/","index");
+        Route::post("/","store");
+        Route::get("/{id}","show");
+        Route::put("/{id}","update");
+        Route::delete("/{id}","destroy");
+    });
+    Route::controller(AuthController::class)->group(function(){
+        Route::get("/user-profile","profile");
+    });
+
 });
 
-Route::controller(CategoriesController::class)->prefix("/categories")->group(function(){
-    Route::get("/","index");
-    Route::post("/","store");
-    Route::get("/{id}","show");
-    Route::put("/{id}","update");
-    Route::delete("/{id}","destroy");
-});
-
-Route::controller(BrandController::class)->prefix("/brands")->group(function(){
-    Route::get("/","index");
-    Route::post("/","store");
-    Route::get("/{id}","show");
-    Route::put("/{id}","update");
-    Route::delete("/{id}","destroy");
-});
-Route::controller(SizesController::class)->prefix("/sizes")->group(function(){
-    Route::get("/","index");
-    Route::post("/","store");
-    Route::get("/{id}","show");
-    Route::put("/{id}","update");
-    Route::delete("/{id}","destroy");
-});
-
-Route::controller(OrdersController::class)->prefix("/orders")->group(function(){
-    Route::get("/","index");
-    Route::post("/","store");
-    Route::get("/{id}","show");
-    Route::get("/{id}/checkout","checkout");
-    Route::put("/{id}","updateStatus");
-    Route::delete("/{id}","destroy");
-});
-
-
-Route::controller(ProductsController::class)->prefix("/products")->group(function(){
-    Route::get("/","index");
-    Route::post("/","store");
-    Route::delete("/stocks/{id}","removeProductStock");
-    Route::get("/{id}","show");
-    Route::get("/{id}/images","productImages");
-    Route::post("/{id}/images","addProductImage");
-    Route::get("/{id}/stocks","productStocks");
-    Route::post("/{id}/stocks","addProductStock");
-    Route::put("/{id}","update");
-    Route::delete("/{id}","destroy");
-});
-
-
-
-
-
-Route::controller(AddressesController::class)->middleware("auth:sanctum")->prefix("/addresses")->group(function(){
-    Route::get("/","index");
-    Route::post("/","store");
-    Route::get("/{id}","show");
-    Route::put("/{id}","update");
-    Route::delete("/{id}","destroy");
-});
-Route::controller(AuthController::class)->middleware('guest')->group(function(){
+Route::controller(AuthController::class)->group(function(){
     Route::post("/login","login");
     Route::post("/register","register");
-});
-
-Route::controller(AuthController::class)->middleware("auth:sanctum")->group(function(){
-    Route::get("/user-profile","profile");
 });
